4e144b0b82c37c92b7ef9f75f023fa7880700a41
Elineora <109291643+Elineora07@users.noreply.github.com>
2022-07-17 21:51:06 +0500

    Add files via upload

---
+++ projectv/dars/new.py
@@ -0,0 +1,495 @@
+# # import math
+# # e = math.e
+# # p = math.pi
+# # 
+# # 
+# # a = 50
+# # b = 60
+# # c = a+b
+# # print(c)
+# print("-------5 chi Misol-----")
+
+# x = float(input("x = "))
+# y = float(input("y = "))
+
+# l = 1.51*math.cos*(x**2)+2*(x**3)
+
+# print("l =" ,l)
+# print("------6 chi Misol-----")
+# y = float(input("y = "))
+
+# m =math.cos*2*y+3.6*e**x
+# print("m =", m)
+# 
+# print("------7 chi Misol-----")
+# m = float(input("m = "))
+# m = (m**2)+2.8*abs(m)+0.55
+
+# print("n =", n)
+
+# print("-----8 chi Misol-----")
+# y = float(input("y = "))
+# 
+# t = math.sqrt(abs(6*(y**2)-0.1*y+4)
+# 
+# print("t =", t)
+
+# print("-----9 chi Misol-----")
+# y = float(input("y = "))
+# x = float(input("x = "))
+# 
+# v = math.log(e,*(y+0.95)+math.sin*(x**4)
+# 
+# print("v =", v)
+# 
+# print("-----10 chi Misol-----")
+# k = float(input("k = "))
+# x = float(input("x = "))
+# 
+# u = (e**y)+7.355*(k**2)+(math.sin**2)*x
+# 
+# print("u=", u)
+# 
+# print("------11 chi Misol----")
+# y = float(input("y = "))
+# x =float(input("x = "))
+# 
+# s = 9.756*(y**7)+2*math.tan*x
+# 
+# print("s =", s)
+# 
+# print("------12 chi Misol-----")
+# t = float(input("t = "))
+# x = float(input("x = "))
+# 
+# k = 7*(t**2)+3*math.sin(x**3)+9.2
+# 
+# print("k =", k)
+# 
+# print("-------13 chi Misol----")
+# y = float(input("y = "))
+# 
+# e = math.sqrt(abs(3*(y**2)+0.5*y+4)
+# 
+# print("e =", e)
+# 
+# print("-------14 chi Misol------")
+# y =float(input("y = "))
+# 
+# r = abs(math.sqrt(math.sin**2)*y+6.835+e**x
+# 
+# print("r =", r)
+# 
+# print("-----15 chi Misol-----")
+# y =float(input("y = "))
+# 
+# h = math.sin*(y**2)-2.8*y+math.sqrt(abs(y))
+# 
+# print("h =", h)
+
+# print("-----16 chi Misol------") 
+# y = float(input("y = "))
+# 
+# s = math.sqrt(math.cos*4*(y**2)+7.151)
+# 
+# print("s =", s)
+# 
+# print("------17 chi Misol-----")
+# y = float(input("y = "))
+# 
+# n = 3*y**2+math.sqrt(y+1)
+# 
+# print("n =", n)
+# 
+# print("-----18 chi Misol-----")
+# y = float(input("y = "))
+# 
+# z = 3*y**2+math.sqrt(y**3)+1)
+# 
+# print("z =", z)
+# 
+# print("------19 chi Misol-----")
+# n = float(input("n = "))
+# y = float(input("y = "))
+# g = float(input("g = "))
+# 
+# p = n*math.sqrt(y**3)+1.09*g
+# 
+# print("g =", g)
+# 
+# print("-----20 chi Misol----")
+# k = float(input("k = "))
+# y = float(input("y = "))
+# x = float(input("x = "))
+# 
+# u = e**(k+y)+math.tan*x*math.sqrt(y)
+# 
+# print("u =", u)
+# 
+# print("------21 chi Misol-----")
+# y = float(input("y = "))
+# h = float(input("h = "))
+# 
+# p = e**(y+5.5)+9.1*(h**3)
+# 
+# print("p =", p)
+# 
+# print("------22 chi Misol-----")
+# u = float(input("u = ")) 
+# y = float(input("y = "))
+# x = float(input("x = "))
+# 
+# t = math.sin*(2*u)*math.log(e,)*(2*(y**2)+math.sqrt(x))
+# 
+# print("t =", t)
+# 
+# print("-----23 chi Misol---")
+# y = float(input("y = "))
+# f = float(input("f = "))
+# 
+# g = e**(2*y)+math.sin(f)
+# 
+# print("g =", g)
+
+# print("------24 chi Misol------")
+# y = float(input("y = "))
+# 
+# f = 2*math.sin*(0.214*(y**5)+1)
+# 
+# print("f =", f)
+# 
+# print("-----25 chi Misol----")
+# y = float(input("y = "))
+# f = float(input("f = "))
+# 
+# g = e**(2*y)+math.sin(f**2)
+# 
+# print("g =", g)
+# 
+# print("-------26 chi Misol---- ")
+# p = float(input("p = "))
+# 
+# z = math.sin*((p**2)+0.4)**3
+# 
+# print("z =", z)
+# 
+# print("-------27 chi Misol------")
+# v = float(input("v = "))
+# y = float(input("y = "))
+# x = float(input("x = "))
+# 
+# w = 1.03*v+e**(2*y)+math.tan(abs(x))
+# 
+# print("w =", w)
+# 
+# print("------28 chi Misol-----")
+# y = float(input("y = "))
+# h = float(input("h = "))
+# 
+# t = e**(y+h)+math.sqrt(abs(6.4*y))
+# 
+# print("t =", t)
+# 
+# print("------29 chi Misol----")
+# y = float(input("y = "))
+# 
+# n = 3*(y**2)+math.sin(abs(y+1))
+# 
+# print("n =", n)
+# 
+# print("-------30 chi Misol-----")
+# y = float(input("y = "))
+# r = float(input("r = "))
+# 
+# w = e**(y+r)+7.2*math.sin*r
+# 
+# print("w =", w)
+
+# print("------1 chi Misol------")
+# y = float(input("y = "))
+# f = float(input("f = "))
+# 
+# g = e**(2*y)+math.sin(f)/math.log(e,(3.8*f))
+# 
+# print("g =", g)
+# 
+# print("------2 chi Misol----")
+# d = float(input("d = "))
+# y = float(input("y = "))
+# 
+# f = math.log(e,)*d+(3.5*d+1)/(math.cos*2*y)
+# 
+# print("f =", f)
+
+# print("-----3 chi Misol---")
+# k = float(input("k = "))
+# y = float(input("y = "))
+# 
+# u = (math.log(e,)*k-y)+(y**4))/(e**y)+2.355*(k**2))
+# 
+# print("u =", u)
+# 
+# print("------4 chi Misol-----")
+# w = float(input("w = "))
+# y = float(input("y = "))
+# 
+# g = (9.33*(w**3)+math.sqrt(w)/(math.log(e,)*(y+3.5)+math.sqrt(y))
+# 
+# print("g =", g)
+# 
+# print("------5 chi Misol------")
+# a = float(input("a = "))
+# t = float(input("t = "))
+# y = float(input("y = "))
+# 
+# d = 7.8*a**2+3.52*t/math.log(e,)*(a+2*y)+e**y
+# 
+# print("d =", d)
+# 
+# print("-----6 chi Misol-----")
+# t = float(input("t = "))
+# r = float(input("r = "))
+# y = float(input("y = "))
+# 
+# w = 4*t**3+math.log(e,)*r/e**(y+r)+7.2*math.sin*r
+# 
+# print("w =", w)
+# 
+# print("-----7 chi Misol----")
+# y = float(input("y = "))
+# n = float(input("n = "))
+# 
+# h = y**2-0.8*y+math.sqrt(y)/23.1*n**2+math.cos*n
+# 
+# print("h =", h )
+
+# print("-----8 chi Misol------")
+# y = float(input("y = "))
+# 
+# r = (math.sqrt(math.sin**2*y+6.835)/math.log(e,)*(y+k)+3*y**2
+# 
+# print("r =", r)
+
+# print("-------9 chi Misol------")
+# y = float(input("y = "))
+# q = float(input("q = "))
+# 
+# e = math.log(e,)*(0.7*y+2*q)/math.sqrt(3*y**2+0.5*y+4)
+# 
+# print("e =", e)
+
+# print("----10 chi Misol----")
+# t = float(input("t = "))
+# l = float(input("l = "))
+# y = float(input("y = "))
+# 
+# k = 2*t**2+3*l+7.2/math.log(e,)*y+e**(2*t)
+# 
+# print("k =", k)
+
+# print("------11 chi Misol-----")
+# x = float(input("x = "))
+# a = float(input("a = "))
+# c = float(input("c = "))
+# 
+# l = (math.sqrt(e**x-math.cos**4*(x**2*a**5)))+math.atan**4*(a-x**5)/e*math.sqrt(abs(a+x*c**4))
+# 
+# print("l =", l)
+
+# print("-----12 chi Misol-----")
+# x = float(input("x = "))
+# c = float(input("c = "))
+# t = float(input("t = "))
+# 
+# l = math.ctg**2*c+(2*x**2+5/math.sqrt(c+t))
+# 
+# print("l =", l)
+# 
+# print("-------13 chi Misol------")
+# y = float(input("y = "))
+# h = float(input("h = "))
+# 
+# a = math.tan*(y**3-h**4)+h**2/math.sin**3*h+y
+# 
+# print("a =", a)
+# 
+# print("------14 chi Misol-----")
+# y = float(input("y = "))
+# x = float(input("x = "))
+# 
+# f = (math.sqrt(2+y)**2)+(math.sqrt(math.sin(y+5)) , 1/7/math.log(e,)*(x+1)-y**3
+# 
+# print("f =", f)
+
+# print("-------15 chi Misol--------")
+# x = float(input("x = "))
+# z = float(input("z = "))
+# y = float(input("y = "))
+# c = float (input("c = "))
+# 
+# g = math.tan*(x**4-6)-math.cos**3*(z+x*y)/math.cos**4*x**3*c**2
+# 
+# print("g =", g)
+
+# print("-------16 chi Misol----")
+# x = float(input("x = "))
+# y = float(input("y = "))
+# t = float(input("t = "))
+# 
+# p = math.sin**3*x+math.log(e,)*(2*y+3*x)/t**e+math.sqrt(x)
+# 
+# print("p =" , p)
+
+# print("-------17 chi Misol-------")
+# x = float(input("x = "))
+# b = float(input("b = "))
+# y = float(input("y = "))
+# 
+# t = (math.sqrt(x+b-a+(math.log(e,)*y))/math.atan*(b+a)
+# 
+# print("t =" , t)
+
+# print("------18 chi Misol------")
+# y = float(input("y = "))
+# t = float(input("t = "))
+# 
+# s = 4.351*y**3+2*t*math.log(e,)*t/math.sqrt(math.cos*2*y+4.351)
+# 
+# print("s =" , s)
+
+# 
+# print("------19 chi Misol-------")
+# a = float(input("a = "))
+# b = float(input("b = "))
+# x = float(input("x = "))
+# y = float(input("y = "))
+# r = float(input("r = "))
+# 
+# d = k**(-a*r*x)-a*math.sqrt(6)-math.cos(3*a*b)/math.sin**2*(a*math.asin*x+math.log(e,)*y)
+# 
+# print("d =" , d)
+# 
+# print("-----20 chi Misol----")
+# y = float(input("y = "))
+# x = float(input("x = "))
+# a = float(input("a = "))
+# b = float(input("b = "))
+# c = float(input("c = "))
+# 
+# u = math.tan**3+math.sin**5*x*math.sqrt(b-c)/math.sqrt(a-b+c)
+# 
+# print("u =", u)
+
+# 
+# print("------8 (1) chi Misol-----")
+# a = int(input("a = "))
+# b = int(input("b = "))
+# 
+# x = abs(a*b)=abc(a)*abc(b)
+# 
+# print("x=", x)
+# 
+# 
+# print("------8 (2) chi Misol------")
+# a = int(input("a = "))
+# b = float(input("b = "))
+# 
+# n = abs(a/b)=abs(a/b)
+# 
+# print("n=", n)
+
+# print("-----9 (1) chi Misol-----")
+# x = float(input("x = ")
+# 
+# x = abs(x**2)-4)/(x+2)
+# 
+# print("x =", x)
+# 
+# print("------9 (2) chi Misol----- ")
+# a = int(input("a = "))
+# b = int(input("b = "))
+# c = int(input("c = "))
+# d = int(input("d = "))
+# e = int(input("e = "))
+# 
+# x = (abs(a)+abs(b)*(b+c)*(abs(s)+d)/abs(d)+e
+# 
+# print("x =", x)
+# 
+# print("------9 (3) chi Misoli----")
+# a = int(input("a = "))
+# b = int(input("b = "))
+# 
+# k = abs(b+3)/(a-2)*(-4)
+# 
+# print("k =", k)
+# 
+# print("-----9 (4) chi Misol---")
+# m = int(input("m = "))
+# n = int(input("n = "))
+# 
+# z = m-(m-n/(-2)*(-5))
+# 
+# print("z =", z)
+
+# print("----9 (5) chi Misol---")
+# p = int(input("p = "))
+# q = int(input("q = "))
+# 
+# l = (-1)*abs(p-(-5)*(-q))+(p+q)*(-2)
+# 
+# print("l =", l)
+
+# 
+# print("-----9 (6) chi Misol----")
+# x = int(input("x = "))
+# y = int(input("y = "))
+# 
+# f = x/(y-1)*(-4)-(x*y+(-3))/(-1)
+# print("f =", f)
+
+
+
+
+
+
+#import math
+# e = math.e
+# p = math.pi 
+# 
+# 
+# a = 50 
+# b = 60
+# 
+# c = a+b
+# print(c)
+# 
+# 
+# print("-----1 chi Misol-----")
+# t = float(input("t = "))
+# l = float(input("l = "))
+# 
+# r = 3 * (t**2) + (l ** 5) + 4.9
+# 
+# 
+# print("r =",r)
+
+# print("-----2 chi Misol-----")
+# 
+# 
+# y = float(input("y = "))
+# 
+# k = math.log(e,((p**2+y**3))+e**p
+# print("k =",k)
+
+#print("------3 ch Misol------")
+
+#n = float(input("n ="))
+#y = float(input("y ="))
+
+#g = n*(y+3.5)+math.sqrt(y)
+
+#print("g = ", g)
+
+
+
